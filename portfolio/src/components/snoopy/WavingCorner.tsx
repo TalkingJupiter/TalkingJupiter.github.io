1516e2b80ce86d303094582e723b4cd3e3e3ffa9
TalkingJupiter <dev.batuhansencer@gmail.com>
2026-01-02 19:58:59 -0600

    v1.4 Updated Snoopy

--- portfolio/src/components/snoopy/WavingCorner.tsx
+++ portfolio/src/components/snoopy/WavingCorner.tsx
@@ -7,8 +7,8 @@ export default function SnoopyWave() {
       {/* <img src="/snoopy/snoopy-fishing.gif" alt="" aria-hidden="true" /> */}
       {/* <img src="/snoopy/snoopy-newyears.gif" alt="" aria-hidden="true" /> */}
       {/* <img src="/snoopy/snoopy-plane.gif" alt="" aria-hidden="true" /> */}
-      <img src="/snoopy/snoopy-tree.gif" alt="" aria-hidden="true" />
-      {/* <img src="/snoopy/snoopy-working.gif" alt="" aria-hidden="true" /> */}
+      {/* <img src="/snoopy/snoopy-tree.gif" alt="" aria-hidden="true" /> */}
+      <img src="/snoopy/snoopy-working.gif" alt="" aria-hidden="true" />
     </div>
   );
 }
